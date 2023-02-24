e15b31495295d0f8f1dccee3f2cf2fae56032a16
LVAK-NapoleonII <124894637+LVAK-NapoleonII@users.noreply.github.com>
2023-02-24 22:27:53 +0700

    24/2

---
+++ wedfilm/wedfilm/Controllers/wedfilmController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wedfilm.Controllers
+{
+    public class wedfilmController : Controller
+    {
+        // GET: wedfilm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult menu()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult GENRE()
+        {
+            return View();
+        }
+        public ActionResult MOVIES()
+        {
+            return View();
+        }
+        public ActionResult SERIES()
+        {
+            return View();
+        }
+        public ActionResult ABOUT()
+        {
+            return View();
+        }
+
+    }
+}
