c384f4e88b24d47d67f33c68dc1b0dd5b2121220
LVAK-NapoleonII <124894637+LVAK-NapoleonII@users.noreply.github.com>
2023-03-03 22:54:15 +0700

    add

--- wedfilm/wedfilm/Controllers/HomeController.cs
+++ wedfilm/wedfilm/Controllers/HomeController.cs
@@ -1,30 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace wedfilm.Controllers
-{
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
-}
+﻿using System.Web.Mvc;namespace wedfilm.Controllers{    public class HomeController : Controller    {        public ActionResult Index()        {            return View();        }        public ActionResult About()        {            ViewBag.Message = "Your application  page.";            return View();        }        public ActionResult Contact()        {            ViewBag.Message = "Your contact page.";            return View();        }    }}
